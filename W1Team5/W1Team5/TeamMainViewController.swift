d7ddcbdc9f705e03b74ee5020882fea6b23b22c8
이재영 <ldc1000@naver.com>
2024-10-22 16:10:01 +0900

    [UI]TeamMainView+VC : 초기 레이아웃 설정
    - Navigation Controller 임베드

---
+++ W1Team5/W1Team5/TeamMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamMainView
+//
+//  Created by t2023-m0072 on 10/21/24.
+//
+
+import UIKit
+
+class TeamMainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
