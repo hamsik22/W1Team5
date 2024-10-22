607840ff7d03a42e824a769fe3eb9394447590cb
이재영 <ldc1000@naver.com>
2024-10-22 17:45:59 +0900

    Feat: add setProfileImageRound
    - profileImageCornerRadius를 통해 profileImageView의 둥글기 값 설정

--- W1Team5/W1Team5/TeamMainViewController.swift
+++ W1Team5/W1Team5/TeamMainViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
+
 class TeamMainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        setProfileImageRound()
     }
     
+    
+    //프로필 이미지 둥글기 값
+    private var profileImageCornerRadius: CGFloat = 15
+    
+    @IBOutlet private var profileImageView1: UIImageView!
+    @IBOutlet private var profileImageView2: UIImageView!
+    @IBOutlet private var profileImageView3: UIImageView!
+    @IBOutlet private var profileImageView4: UIImageView!
+    @IBOutlet private var profileImageView5: UIImageView!
+    @IBOutlet private var profileImageView6: UIImageView!
+    
+    private var allProfileImageView: [UIImageView] { [profileImageView1,
+                                                      profileImageView2,
+                                                      profileImageView3,
+                                                      profileImageView4,
+                                                      profileImageView5,
+                                                      profileImageView6]
+    }
+    
+}
+
+
+extension TeamMainViewController {
+    
+    private func setProfileImageRound() {
+        allProfileImageView.forEach { imageView in
+            
+            imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = profileImageCornerRadius
+            
+            imageView.layer.maskedCorners = CACornerMask(arrayLiteral:.layerMaxXMaxYCorner,
+                                                         .layerMinXMinYCorner,
+                                                         .layerMinXMaxYCorner,
+                                                         .layerMaxXMinYCorner)
+        }
+    }
     
 }
