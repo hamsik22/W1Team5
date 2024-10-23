c6d84b31b8b891d40f19010c275d35dbb468648a
Quaker-Lee <tkacl2010@gmail.com>
2024-10-23 16:48:04 +0900

    Initial Commit

---
+++ W1Team5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W1Team5
+//
+//  Created by 이재건 on 10/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ W1Team5Tests/W1Team5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  W1Team5Tests.swift
+//  W1Team5Tests
+//
+//  Created by 이재건 on 10/23/24.
+//
+
+import Testing
+@testable import W1Team5
+
+struct W1Team5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
