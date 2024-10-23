6fa57d84b965a83fb9973e80222a3b3df0348c13
name-mun <tains9153@gmail.com>
2024-10-23 21:59:22 +0900

    [UI] MunFirstView+VC : 초기 레이아웃 설정

---
+++ W1Team5/W1Team5/MunFirstView/MunData.swift
@@ -0,0 +1,21 @@
+//
+//  MunData.swift
+//  W1Team5
+//
+//  Created by mun on 10/23/24.
+//
+
+struct MunData {
+    let name: String!
+    let introduce: String
+    let mbti: String
+    let strengths: [String]
+    let style: [String]
+    let cooperation: [String]
+    let blogURL: String
+    let githubURL: String
+    let profileImage: String
+    let photo: [String]
+    let content: [String]
+    let time: [String]
+}
